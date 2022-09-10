8c5b4c61e2d06cfc5114ce27ed61416c048ffbbe
Restyled.io <commits@restyled.io>
2022-09-10 21:43:41 +0000

    Restyled by clang-format

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-nameArray=['<h1 class="text-center">Welcome. <br /> Mrmouse</h1>', '<h1 class="text-center">Hello. <br /> Mrmouse</h1>', '<h1 class="text-center">Cheezy. <br /> Mrmouse</h1>'];
-id=1
-function nameChange(){
-    document.getElementById('titleDiv').innerHTML=nameArray[id];
-    if(id==3){
-        id=0;
-        document.getElementById('titleDiv').innerHTML=nameArray[id];
-    }
-    id += 1
+nameArray = [
+  '<h1 class="text-center">Welcome. <br /> Mrmouse</h1>',
+  '<h1 class="text-center">Hello. <br /> Mrmouse</h1>',
+  '<h1 class="text-center">Cheezy. <br /> Mrmouse</h1>'
+];
+id = 1
+function nameChange() {
+  document.getElementById('titleDiv').innerHTML = nameArray[id];
+  if (id == 3) {
+    id = 0;
+    document.getElementById('titleDiv').innerHTML = nameArray[id];
+  }
+  id += 1
 }
