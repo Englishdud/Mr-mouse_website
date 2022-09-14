c5d289be72ba8f37ae0e98a9f07b2ff68ecf3e71
Englishdud <andrew_toronto@outlook.com>
2022-09-13 22:10:34 -0400

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,3 +1,11 @@
+Notification.requestPermission().then(perm => {
+    if (perm === 'granted') {
+        console.log('Notification permission granted.');
+    } else {
+        console.log('Unable to get permission to notify.');
+    }
+})
+// Divider
 nameArray=['<h1 class="text-center">Welcome. <br /> Mrmouse</h1>', '<h1 class="text-center">Hello. <br /> Mrmouse</h1>', '<h1 class="text-center">Cheezy. <br /> Mrmouse</h1>'];
 id=1
 function nameChange(){
