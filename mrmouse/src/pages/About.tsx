c07480701148fdfef592d146464e8f4b91ae009a
Englishdud <andrew_toronto@outlook.com>
2022-10-21 22:08:02 -0400

    vite added

---
+++ mrmouse/src/pages/About.tsx
@@ -0,0 +1,5 @@
+export function About() {
+    return (
+      <p>Created by;<span><a href="https://jacobvarughese.me/Andrew-Jacob-Portfolio">Andrew</a></span></p>
+    )
+  }
---
+++ mrmouse/src/pages/Store.tsx
@@ -0,0 +1,5 @@
+export function Store() {
+    return (
+      <p>Created by;<span><a href="https://jacobvarughese.me/Andrew-Jacob-Portfolio">Andrew</a></span></p>
+    )
+  }
